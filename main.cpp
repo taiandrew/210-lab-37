fed617700c0dc87fc66ad00c2d4bb64719c6feee
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-12-01 20:38:57 -0800

    Init (no starter files)

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// COMSC 210 | Lab 37 | 2 DEC 2025
+// IDE: VS Code
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+
+using namespace std;
+
+// Fn prototypes
+int sum_ascii(const string &str);
+
+int main() {
+
+    return 0;
+}
+
