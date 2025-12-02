bc6689d941679f9619a198c9c0750d14e0a12e43
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-12-01 20:43:48 -0800

    sum_ascii and tester

--- main.cpp
+++ main.cpp
@@ -10,9 +10,35 @@ using namespace std;
 
 // Fn prototypes
 int sum_ascii(const string &str);
+void tester();
 
+// -------------------------------------------
+// Main
+// -------------------------------------------
 int main() {
+
+    tester();
 
     return 0;
 }
 
+// -------------------------------------------
+// Programmer fns
+// -------------------------------------------
+
+int sum_ascii(const string &str) {
+    // Arg: str to convert to ASCII sum
+    // Ret: int: sum of ASCII values of all chars in str
+
+    int sum = 0;
+    for (char ch : str) {
+        sum += (int) ch;
+    }
+    return sum;
+}
+
+void tester() {
+    cout << "A" << sum_ascii("A") << endl; // 65
+    cout << "AB" << sum_ascii("AB") << endl; // 65 + 66 = 131
+    cout << "q!" << sum_ascii("q!") << endl; // 33 + 113 = 146
+}
